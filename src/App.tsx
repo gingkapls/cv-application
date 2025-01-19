aba8a6eb53a6a0ba9b053af3a8b535895419ebd9
gin <73906888+gingkapls@users.noreply.github.com>
2025-01-20 02:20:30 +0530

    chore: add driver code

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import './App.css';
 import ContactForm, { ContactDetails } from './Components/ContactForm';
 import { ContactPreview } from './Components/ContactPreview';
+import EducationForm, { EducationDetails } from './Components/EducationForm';
 
 function App() {
   const [contactDetails, setContactDetails] = useState<ContactDetails>({
@@ -12,13 +13,43 @@ function App() {
     gmail: '',
   });
 
+  const [educationDetails, setEducationDetails] = useState<EducationDetails[]>([
+    {
+      id: '1',
+      collegeName: 'Test College',
+      degree: 'MCA',
+      gpa: '9.4',
+      startDate: new Date(),
+      endDate: new Date(),
+    },
+    {
+      id: '2',
+      collegeName: 'Test College',
+      degree: 'BCA',
+      gpa: '9.4',
+      startDate: new Date(),
+      endDate: new Date(),
+    },
+  ]);
+
   return (
     <>
       <ContactForm
         contactDetails={contactDetails}
         setContactDetails={setContactDetails}
       />
+
       <ContactPreview contactDetails={contactDetails} />
+      {educationDetails.map((details) => {
+        return (
+          <EducationForm
+            id={details.id}
+            key={details.id}
+            educationDetails={educationDetails}
+            setEducationDetails={setEducationDetails}
+          />
+        );
+      })}
     </>
   );
 }
