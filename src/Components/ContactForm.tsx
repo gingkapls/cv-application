f6f531550ab262a1e5ef6d62e050ba712833738e
gin <73906888+gingkapls@users.noreply.github.com>
2025-01-19 15:05:11 +0530

    feat(ContactForm): init

---
+++ src/Components/ContactForm.tsx
@@ -0,0 +1,91 @@
+import {
+  ChangeEvent,
+  ChangeEventHandler,
+  Dispatch,
+  SetStateAction,
+} from 'react';
+
+interface InputProps {
+  name: string;
+  label: string;
+  value: string;
+  onChange: ChangeEventHandler;
+  type?: string,
+}
+
+function Input({ name, label, value, type = 'email', onChange }: InputProps) {
+  return (
+    <>
+      <label htmlFor={name}>{label}</label>
+      <input
+        name={name}
+        id={name}
+        type={type}
+        value={value}
+        onChange={onChange}
+      />
+    </>
+  );
+}
+
+export interface ContactDetails {
+  fullName: string;
+  phoneNumber?: string;
+  github?: string;
+  linkedIn?: string;
+  gmail?: string;
+}
+
+interface ContactFormProps {
+  contactDetails: ContactDetails;
+  id?: string;
+  setContactDetails: Dispatch<SetStateAction<ContactDetails>>;
+}
+
+function ContactForm({ contactDetails, setContactDetails }: ContactFormProps) {
+  function handleFieldChange(e: ChangeEvent<HTMLInputElement>) {
+    const field = e.target.name;
+    const value = e.target.value;
+    const newDetails = { ...contactDetails, [field]: value };
+    setContactDetails(newDetails);
+  }
+
+  return (
+    <form className='contact-details' action=''>
+      <h3>Personal Details</h3>
+      <Input
+        label='Full Name: '
+        name='fullName'
+        value={contactDetails.fullName}
+        onChange={handleFieldChange}
+      />
+      <Input
+        label='Phone Number'
+        name='phoneNumber'
+        value={contactDetails.phoneNumber || ''}
+        onChange={handleFieldChange}
+      />
+      <Input
+        label='Github Profile'
+        name='github'
+        value={contactDetails.github || ''}
+        onChange={handleFieldChange}
+      />
+      <Input
+        label='LinkedIn Profile'
+        name='linkedIn'
+        value={contactDetails.linkedIn || ''}
+        onChange={handleFieldChange}
+      />
+      <Input
+        label='E-mail address'
+        type="email"
+        name='gmail'
+        value={contactDetails.gmail || ''}
+        onChange={handleFieldChange}
+      />
+    </form>
+  );
+}
+
+export default ContactForm;
