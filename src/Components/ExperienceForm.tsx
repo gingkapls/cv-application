87211b4f882990f4730a1c48317e89367af1336b
gin <73906888+gingkapls@users.noreply.github.com>
2025-01-22 01:52:31 +0530

    feat(ExperienceForm): init component

---
+++ src/Components/ExperienceForm.tsx
@@ -0,0 +1,162 @@
+import { ChangeEvent, Dispatch, FormEvent, SetStateAction } from 'react';
+import Input from './Input';
+import TextArea from './TextArea';
+import { DateString, UUIDString } from '../lib/types';
+import generateUniqueId from '../lib/uniqueId';
+
+export class ExperienceDetails {
+  id: UUIDString;
+  orgName: string;
+  jobTitle: string;
+  location: string;
+  description: string;
+  #startDate: Date = new Date();
+  #endDate: Date = new Date();
+  [key: string]: string | (() => ExperienceDetails);
+
+  constructor({
+    id = generateUniqueId(),
+    orgName = '',
+    jobTitle = '',
+    location = '',
+    description = '',
+    startDate = new Date(),
+    endDate = new Date(),
+  } = {}) {
+    this.id = id;
+    this.orgName = orgName;
+    this.jobTitle = jobTitle;
+    this.location = location;
+    this.description = description;
+    this.startDate = startDate;
+    this.endDate = endDate;
+  }
+
+  set startDate(date: Date | DateString) {
+    this.#startDate = date instanceof Date ? date : this.#parseDate(date);
+  }
+
+  get startDate(): DateString {
+    return this.#formatDate(this.#startDate);
+  }
+
+  set endDate(date: Date | DateString) {
+    this.#endDate = date instanceof Date ? date : this.#parseDate(date);
+  }
+
+  get endDate(): DateString {
+    return this.#formatDate(this.#endDate);
+  }
+
+  clone(): ExperienceDetails {
+    return new ExperienceDetails({
+      id: this.id,
+      orgName: this.orgName,
+      jobTitle: this.jobTitle,
+      location: this.location,
+      description: this.description,
+      startDate: this.#startDate,
+      endDate: this.#endDate,
+    });
+  }
+
+  #formatDate(date: Date): DateString {
+    const year = date.getFullYear();
+    const month = (date.getMonth() + 1).toString().padStart(2, '0'); // 0 indexed month
+    const day = date.getDate().toString().padStart(2, '0');
+    return `${year}-${month}-${day}`;
+  }
+
+  #parseDate(dateString: DateString): Date {
+    return new Date(dateString);
+  }
+}
+
+interface ExperienceFormProps {
+  id: string;
+  experienceDetails: ExperienceDetails[];
+  setExperienceDetails: Dispatch<SetStateAction<ExperienceDetails[]>>;
+}
+
+function ExperienceForm({
+  id,
+  experienceDetails,
+  setExperienceDetails,
+}: ExperienceFormProps) {
+  const detailIndex = experienceDetails.findIndex(
+    (detail: ExperienceDetails) => detail.id === id
+  );
+
+  const { orgName, jobTitle, location, description, startDate, endDate } =
+    experienceDetails.at(detailIndex)!;
+
+  function handleFieldChange(e: ChangeEvent<HTMLInputElement>) {
+    const field = e.target.name;
+    const value = e.target.value;
+    const originalDetails = experienceDetails.at(detailIndex)!;
+    const newDetails = originalDetails.clone();
+    newDetails[field] = value;
+
+    const newEducationDetails = experienceDetails.map((details) => {
+      if (details.id === id) return newDetails;
+      return details;
+    });
+    setExperienceDetails(newEducationDetails);
+  }
+
+  function handleSubmit(e: FormEvent) {
+    e.preventDefault();
+    if (!(e.target instanceof HTMLFormElement)) return;
+
+    const formData = new FormData(e.target);
+    console.log(formData);
+  }
+  return (
+    <form className='experience-form' onSubmit={handleSubmit}>
+      <Input
+        label='Organization Name'
+        name='orgName'
+        value={orgName}
+        onChange={handleFieldChange}
+      />
+
+      <Input
+        label='Job Title'
+        name='jobTitle'
+        value={jobTitle}
+        onChange={handleFieldChange}
+      />
+
+      <TextArea
+        label='Job Description'
+        name='description'
+        value={description}
+        onChange={handleFieldChange}
+      />
+
+      <Input
+        label='Location'
+        name='location'
+        value={location}
+        onChange={handleFieldChange}
+      />
+      <Input
+        label='Start Date: '
+        type='Date'
+        name='startDate'
+        value={startDate}
+        onChange={handleFieldChange}
+      />
+      <Input
+        label='End Date: '
+        type='Date'
+        name='endDate'
+        value={endDate}
+        onChange={handleFieldChange}
+      />
+      <button type='submit'>Save</button>
+    </form>
+  );
+}
+
+export default ExperienceForm;
