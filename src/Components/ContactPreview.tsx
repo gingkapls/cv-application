e048a12f8a3788bc2c5e39f43f16e039e5e3302d
gin <73906888+gingkapls@users.noreply.github.com>
2025-01-24 01:33:20 +0530

    feat(ContactPreview): open links in new tabs

--- src/Components/ContactPreview.tsx
+++ src/Components/ContactPreview.tsx
@@ -1,10 +1,6 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { ContactDetails } from './ContactForm';
-import {
-  faEnvelope,
-  faHouse,
-  faPhone,
-} from '@fortawesome/free-solid-svg-icons';
+import { faEnvelope, faPhone } from '@fortawesome/free-solid-svg-icons';
 import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
 function ContactPreview({
@@ -18,19 +14,39 @@ function ContactPreview({
       <h2 className='full-name'>{fullName}</h2>
       <span className='phone-number'>
         <FontAwesomeIcon icon={faPhone} />
-        <a href={`tel: ${phoneNumber}`}>{phoneNumber}</a>
+        <a
+          target='_blank'
+          href={`tel: ${phoneNumber}`}
+          rel='noopener noreferrer'
+        >
+          {phoneNumber}
+        </a>
       </span>
       <span className='github'>
         <FontAwesomeIcon icon={faGithub} />
-        <a href={`https://github.com/${github}`}>{github}</a>
+        <a
+          target='_blank'
+          href={`https://github.com/${github}`}
+          rel='noopener noreferrer'
+        >
+          {github}
+        </a>
       </span>
       <span className='linkedIn'>
         <FontAwesomeIcon icon={faLinkedin} />
-        <a href={`https://linkedin.com/in/${linkedIn}`}>{linkedIn}</a>
+        <a
+          target='_blank'
+          href={`https://linkedin.com/in/${linkedIn}`}
+          rel='noopener noreferrer'
+        >
+          {linkedIn}
+        </a>
       </span>
       <span className='gmail'>
         <FontAwesomeIcon icon={faEnvelope} />
-        <a href={`mailto:${gmail}`}>{gmail}</a>
+        <a target='_blank' href={`mailto:${gmail}`} rel='noopener noreferrer'>
+          {gmail}
+        </a>
       </span>
     </address>
   );
