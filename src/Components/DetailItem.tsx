a49bfc12406eef65d906ff9fd646e88823f23aea
gin <73906888+gingkapls@users.noreply.github.com>
2025-01-23 21:38:45 +0530

    feat(DetailItem): add hide toggle

--- src/Components/DetailItem.tsx
+++ src/Components/DetailItem.tsx
@@ -1,16 +1,22 @@
 import { Dispatch, SetStateAction } from 'react';
 import { UUIDString } from '../lib/types';
+
 interface DetailItemProps {
   id: UUIDString;
   title: string;
   setActiveId: Dispatch<SetStateAction<UUIDString | null>>;
+  toggleHide: (id: UUIDString) => void;
 }
 
-function DetailItem({ id, title, setActiveId }: DetailItemProps) {
+function DetailItem({ id, title, setActiveId, toggleHide }: DetailItemProps) {
   return (
     <li className='detail-item'>
-      <button className='item-title' onClick={() => setActiveId(id)}>{title}</button>
-      <button className='item-hide'>X</button>
+      <button className='item-title' onClick={() => setActiveId(id)}>
+        {title}
+      </button>
+      <button className='item-hide' onClick={() => toggleHide(id)}>
+        X
+      </button>
     </li>
   );
 }
