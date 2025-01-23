8f62906c28a438451d1d5438b5e9f6d1040b8394
gin <73906888+gingkapls@users.noreply.github.com>
2025-01-24 01:21:18 +0530

    feat(ContactPreview): use semantic html, add icons

--- src/Components/ContactPreview.tsx
+++ src/Components/ContactPreview.tsx
@@ -1,4 +1,11 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { ContactDetails } from './ContactForm';
+import {
+  faEnvelope,
+  faHouse,
+  faPhone,
+} from '@fortawesome/free-solid-svg-icons';
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
 function ContactPreview({
   contactDetails,
@@ -7,13 +14,25 @@ function ContactPreview({
 }) {
   const { fullName, phoneNumber, github, linkedIn, gmail } = contactDetails;
   return (
-    <div className='contact-preview detail-preview'>
+    <address className='contact-preview detail-preview'>
       <h2 className='full-name'>{fullName}</h2>
-      <span className='phone-number'>{phoneNumber}</span>
-      <span className='github'>{github}</span>
-      <span className='linkedIn'>{linkedIn}</span>
-      <span className='gmail'>{gmail}</span>
-    </div>
+      <span className='phone-number'>
+        <FontAwesomeIcon icon={faPhone} />
+        <a href={`tel: ${phoneNumber}`}>{phoneNumber}</a>
+      </span>
+      <span className='github'>
+        <FontAwesomeIcon icon={faGithub} />
+        <a href={`https://github.com/${github}`}>{github}</a>
+      </span>
+      <span className='linkedIn'>
+        <FontAwesomeIcon icon={faLinkedin} />
+        <a href={`https://linkedin.com/in/${linkedIn}`}>{linkedIn}</a>
+      </span>
+      <span className='gmail'>
+        <FontAwesomeIcon icon={faEnvelope} />
+        <a href={`mailto:${gmail}`}>{gmail}</a>
+      </span>
+    </address>
   );
 }
 
