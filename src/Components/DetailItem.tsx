40334040181c60d81b36039fb0245e6211a83272
gin <73906888+gingkapls@users.noreply.github.com>
2025-01-27 22:21:34 +0530

    feat(DetailItem): allow moving items up and down, increase icon size

--- src/Components/DetailItem.tsx
+++ src/Components/DetailItem.tsx
@@ -1,6 +1,13 @@
 import { Dispatch, SetStateAction } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
+import {
+  faArrowDown,
+  faArrowUp,
+  faCircleDown,
+  faCircleUp,
+  faEye,
+  faEyeSlash,
+} from '@fortawesome/free-solid-svg-icons';
 import { UUIDString } from '../lib/uniqueId';
 import { EducationDetails } from './EducationForm';
 import { ExperienceDetails } from './ExperienceForm';
@@ -33,6 +40,36 @@ function toggleHide<T extends EnumerableDetails>(
   );
 }
 
+function moveUp<T extends EnumerableDetails>(
+  index: number,
+  details: T[],
+  setDetails: Dispatch<SetStateAction<T[]>>
+) {
+  if (index === 0) return;
+
+  const newDetails = [...details];
+  const prevEl = newDetails[index - 1];
+  newDetails[index - 1] = newDetails[index];
+  newDetails[index] = prevEl;
+
+  setDetails(newDetails);
+}
+
+function moveDown<T extends EnumerableDetails>(
+  index: number,
+  details: T[],
+  setDetails: Dispatch<SetStateAction<T[]>>
+) {
+  if (index >= details.length - 1) return;
+
+  const newDetails = [...details];
+  const nextEl = newDetails[index + 1];
+  newDetails[index + 1] = newDetails[index];
+  newDetails[index] = nextEl;
+
+  setDetails(newDetails);
+}
+
 function DetailItem<T extends EnumerableDetails>({
   id,
   title,
@@ -41,8 +78,30 @@ function DetailItem<T extends EnumerableDetails>({
   setDetails,
   setActiveId,
 }: DetailItemProps<T>) {
+  const index = details.findIndex((detail) => detail.id === id);
+  const isFirst = index === 0;
+  const isLast = index === details.length - 1;
+
   return (
     <li className={`detail-item${isVisible ? ' visible' : ''}`}>
+      <div className='btn-arrow-container'>
+        {!isFirst && (
+          <button
+            className='btn-arrow'
+            onClick={() => moveUp(index, details, setDetails)}
+          >
+            <FontAwesomeIcon icon={faArrowUp} className='fa-xl' />
+          </button>
+        )}
+        {!isLast && (
+          <button
+            className='btn-arrow'
+            onClick={() => moveDown(index, details, setDetails)}
+          >
+            <FontAwesomeIcon icon={faArrowDown} className='fa-xl' />
+          </button>
+        )}
+      </div>
       <button className='item-title' onClick={() => setActiveId(id)}>
         {title}
       </button>
@@ -50,7 +109,10 @@ function DetailItem<T extends EnumerableDetails>({
         className='item-hide'
         onClick={() => toggleHide(id, details, setDetails)}
       >
-        <FontAwesomeIcon icon={isVisible ? faEye : faEyeSlash} />
+        <FontAwesomeIcon
+          icon={isVisible ? faEye : faEyeSlash}
+          className='fa-xl'
+        />
       </button>
     </li>
   );
