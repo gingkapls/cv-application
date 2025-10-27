757559ccfe3cd5e9ef3d113020b3b5e0c615a249
gin <73906888+gingkapls@users.noreply.github.com>
2025-10-27 17:37:36 +0000

    feat: Enable blank lines to act as bullet separators in textarea

--- src/Components/PreviewItemDescription.tsx
+++ src/Components/PreviewItemDescription.tsx
@@ -4,13 +4,15 @@ import { ProjectDetails } from './ProjectForm';
 type DescriptiveDetails = ExperienceDetails | ProjectDetails;
 
 function PreviewItemDescription({ details }: { details: DescriptiveDetails }) {
+  // Trim the whitespace from both ends
+  // If the a line is empty (blank line) don't include it
   return (
     <ul className='description'>
       {details.description
         .trim()
         .split('\n')
-        .map((item, idx) => (
-          <li key={item + idx}>{item}</li>
+        .flatMap((item, idx) => (
+          item.length !== 0 ? <li key={item + idx}>{item}</li> : []
         ))}
     </ul>
   );
