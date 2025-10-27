655384106cec4d3432a606ccb83588dfcbab8f18
gin <73906888+gingkapls@users.noreply.github.com>
2025-10-27 17:29:03 +0000

    fix: Switch from GET to POST requests to overleaf
    Fixes cases where the codegen and thus URI was too long

--- src/Components/TexActions.tsx
+++ src/Components/TexActions.tsx
@@ -7,6 +7,7 @@ import { ProjectDetails } from './ProjectForm';
 import { SkillsDetails } from './SkillsForm';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faLeaf } from '@fortawesome/free-solid-svg-icons';
+import { FormEvent, useRef } from 'react';
 
 interface TexActionsProps {
   contactDetails: ContactDetails;
@@ -17,23 +18,36 @@ interface TexActionsProps {
   isAnonymized: boolean;
 }
 function TexActions(props: TexActionsProps) {
+  const texRef = useRef<null | HTMLInputElement>(null);
+
   function handleCopy() {
     const text = generateTexCode(props);
     navigator.clipboard.writeText(text);
   }
 
-  function handleOpen() {
-    const URL = 'https://www.overleaf.com/docs?snip_uri=';
-    const snip_uri = 'data:application/x-tex;base64,';
-    const b64Tex = Base64.encode(generateTexCode(props));
-    window.open(URL + snip_uri + b64Tex, '_blank');
+  function handleSubmit() {
+    if (texRef.current === null) {
+      return console.error('The LaTeX code did not export properly');
+    }
+
+    const texCode = generateTexCode(props);
+    texRef.current.value = texCode;
+    return true;
   }
 
   return (
     <div className='tex-actions-container btn-container'>
-      <button className='btn-open-overleaf btn' onClick={handleOpen}>
-        Open in Overleaf <FontAwesomeIcon icon={faLeaf} className='fa-xl' />
-      </button>
+      <form
+        action='https://www.overleaf.com/docs'
+        method='POST'
+        target='_blank'
+        onSubmit={handleSubmit}
+      >
+        <input ref={texRef} type='hidden' name='snip' />
+        <button className='btn-open-overleaf btn' type='submit'>
+          Open in Overleaf <FontAwesomeIcon icon={faLeaf} className='fa-xl' />
+        </button>
+      </form>
       <button className='btn-copy-tex btn' onClick={handleCopy}>
         Copy as LaTeX
       </button>
